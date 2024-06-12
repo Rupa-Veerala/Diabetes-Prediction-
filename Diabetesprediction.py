b5e12f4229366b91629c3d621a89a7b555b7558a
Veerala-Rupa <veeralarupa@gmail.com>
2024-06-12 16:45:57 +0530

    Prediction

--- Diabetesprediction.py
+++ Diabetesprediction.py
@@ -36,4 +36,35 @@ scaler.fit(X)
 standardized_data=scaler.transform(X)
 print(standardized_data)
 
+X = standardized_data
+Y = diabetes_dataset['Outcome']
 
+print(X)
+print(Y)
+
+#split train and test data
+#0.2 means 20% of test data based on y
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2,stratify=Y,random_state=2)
+print(X.shape,X_train.shape,X_test.shape)
+
+#Training the model
+classifier = svm.SVC(kernel='linear')
+#Training the support vector machine classifier
+classifier.fit(X_train,Y_train)
+
+#Model evaluation
+#Accuracy score
+#accuracy score from the training data
+X_train_prediction = classifier.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction,Y_train)
+print('Accuracy score of the training data :',training_data_accuracy)
+
+#accuracy score from the training data
+X_test_prediction = classifier.predict(X_test)
+test_data_accuracy = accuracy_score(X_test_prediction,Y_test)
+print('Accuracy score of the test data :',test_data_accuracy)
+
+#Making a predictive system
+input_data = ()
+
+#
