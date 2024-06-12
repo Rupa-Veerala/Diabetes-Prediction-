04e6c463118a165436da442f504cc2662ac05200
Veerala-Rupa <veeralarupa@gmail.com>
2024-06-12 17:04:29 +0530

    Diabetes Prediction

--- Diabetesprediction.py
+++ Diabetesprediction.py
@@ -65,6 +65,43 @@ test_data_accuracy = accuracy_score(X_test_prediction,Y_test)
 print('Accuracy score of the test data :',test_data_accuracy)
 
 #Making a predictive system
-input_data = ()
+input_data = (4,110,92,0,0,37.6,0.191,30)
 
-#
+#changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+#reshape the array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+#standardize the input data 
+std_data = scaler.transform(input_data_reshaped)
+print(std_data)
+
+prediction = classifier.predict(std_data)
+print(prediction)
+
+if (prediction[0] == 0):
+    print('The person is not diabetic')
+else:
+    print('The person is diabetic')
+
+#check for label 1
+input_data = (5,166,72,19,175,25.8,0.587,51)
+
+#changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+#reshape the array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+#standardize the input data 
+std_data = scaler.transform(input_data_reshaped)
+print(std_data)
+
+prediction = classifier.predict(std_data)
+print(prediction)
+
+if (prediction[0] == 0):
+    print('The person is not diabetic')
+else:
+    print('The person is diabetic')
